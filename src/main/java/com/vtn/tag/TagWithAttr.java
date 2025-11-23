b876258bba3aa8734ef82db90e5562e1cbf8e359
Victor Nguyen <>
2025-11-23 13:34:44 +0700

    # upgrade libs

--- src/main/java/com/vtn/tag/TagWithAttr.java
+++ src/main/java/com/vtn/tag/TagWithAttr.java
@@ -4,16 +4,13 @@ import jakarta.servlet.jsp.tagext.SimpleTagSupport;
 import lombok.Setter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Setter
 public class TagWithAttr extends SimpleTagSupport {
     private String value;
 
     public void doTag() throws IOException {
-        if (value != null) {
-            getJspContext().getOut().println(value);
-        } else {
-            getJspContext().getOut().println("Hello TagWithAttr Tag!");
-        }
+        getJspContext().getOut().println(Objects.requireNonNullElse(value, "Hello TagWithAttr Tag!"));
     }
 }
